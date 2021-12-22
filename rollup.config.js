fee6500e1684521a32dd44e1c977f69932cdb68b
toolbuilder <k_d_hudson@yahoo.com>
2021-12-22 09:06:16 -0500

    chore: removed unneeded rollup-plugin-resolve dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import resolve from '@rollup/plugin-node-resolve'
-
 /**
  * Builds the CommonJS code used to support CommonJS users. Since the package is stateless,
  * the CommonJS code can be completely separate without creating a dual package hazard.
@@ -12,9 +10,6 @@ export default [
     output: {
       dir: 'cjs',
       format: 'cjs'
-    },
-    plugins: [
-      resolve()
-    ]
+    }
   }
 ]
